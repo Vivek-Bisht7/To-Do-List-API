7a541ed96046edae8e71a7230f779a99b0ab0108
Vivek <vivekbisht4320@gmail.com>
2025-07-08 22:36:04 +0530

    Added authentication

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const dbConnection = require('./config/task_connection');
 const taskRoutes = require('./routes/task_routes');
 const userRoutes = require('./routes/user_routes');
+require('dotenv').config()
 
 dbConnection("mongodb://127.0.0.1:27017/db");
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
